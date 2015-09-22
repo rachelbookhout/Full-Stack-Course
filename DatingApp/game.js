ad726b1536e00797ec693d8b7b2f5ca62d4fee71
rachelbookhout <rbookhou@gmail.com>
2015-09-21 21:58:45 -0400

    outlined next assignment

---
+++ DatingApp/game.js
@@ -0,0 +1,6 @@
+//prompt the user to pick a color
+//there is an array of colors to pick from
+//if they don't pick them, alert them and prompt them to pick again
+//if it is, you need to prompt them if it is abc higher or lower
+//when they get it right, you tell them congrats, how many times they guesses
+//and the background of the page changes to the color you picked
