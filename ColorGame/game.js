7590558d88d7eeb5c07772c2b293a31cc7b80edc
rachelbookhout <rbookhou@gmail.com>
2015-09-22 22:29:39 -0400

    created loop layout

--- ColorGame/game.js
+++ ColorGame/game.js
@@ -2,9 +2,15 @@ var target_index = Math.floor((Math.random() * 6) + 1);
 var colors = ["purple", "pink", "yellow", "red", "blue", "black"];
 var target = colors[target_index]
 //prompt the user to pick a color
-prompt("I'm thinking of one of these colors /n purple, pink, yellow, red, blue, black /n  What color am I thinking of?");
+var guess_input = prompt("I'm thinking of one of these colors - purple, pink, yellow, red, blue, black. What color am I thinking of?");
 //there is an array of colors to pick from
 //if they don't pick them, alert them and prompt them to pick again
+while (guess_input != target)
+{
+  if (){}
+  else if{}
+  else{}
+}
 //if it is, you need to prompt them if it is abc higher or lower
 //when they get it right, you tell them congrats, how many times they guesses
 //and the background of the page changes to the color you picked
