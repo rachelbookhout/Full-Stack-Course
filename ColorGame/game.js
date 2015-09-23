5aec7f7a640d8a308964341edab83abe5795200f
rachelbookhout <rbookhou@gmail.com>
2015-09-22 22:20:47 -0400

    files were in wrong folder

--- ColorGame/game.js
+++ ColorGame/game.js
@@ -1,4 +1,8 @@
+var target_index = Math.floor((Math.random() * 6) + 1);
+var colors = ["purple", "pink", "yellow", "red", "blue", "black"];
+var target = colors[target_index]
 //prompt the user to pick a color
+prompt("I'm thinking of one of these colors /n purple, pink, yellow, red, blue, black /n  What color am I thinking of?");
 //there is an array of colors to pick from
 //if they don't pick them, alert them and prompt them to pick again
 //if it is, you need to prompt them if it is abc higher or lower
