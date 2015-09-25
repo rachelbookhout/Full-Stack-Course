5aa74a8511133bf34554101cc8d45782b4413b04
rachelbookhout <rbookhou@gmail.com>
2015-09-25 07:29:57 -0400

    works

--- ColorGame/game.js
+++ ColorGame/game.js
@@ -1,15 +1,16 @@
-var target_index = Math.floor((Math.random() * 6) + 1);
-var colors = ["purple", "pink", "yellow", "red", "blue", "black"];
+var target_index = Math.floor((Math.random() * 10) + 1);
+var colors = ["cyan", "blue", "gold", "grey", "green", "magenta", "orange", "red", "white", "yellow"];
 var target = colors[target_index]
-var finshed = false;
+var finished = false;
 var numberOfGuesses = 0
 
 function do_game(){
   while (!finished)
   {
-    var guess_input = prompt("I'm thinking of one of these colors \n\n" +
+    var answer = prompt("I'm thinking of one of these colors \n\n" +
                           " blue, cyan, gold, gray, green, magenta, orange, red, white, yellow\n\n"+
                           " What color am I thinking of?");
+    guess_input = String(answer);
     numberOfGuesses+=1
     check_guess();
   }
@@ -24,20 +25,23 @@ function check_guess(){
     else
     {
       if (guess_input > target)
-      { alert("Sorry, your guess is not correct\n\n"
+      {
+        alert("Sorry, your guess is not correct\n\n"
           + "Hint: your color is alphabetically higher than mine\n\n" +
             "Please try again");
       }
     else if (target > guess_input)
-     {alert("Sorry, your guess is not correct\n\n"
+      {
+        alert("Sorry, your guess is not correct\n\n"
           + "Hint: your color is alphabetically lower than mine\n\n" +
             "Please try again");
     }
     else
     {
-      body.style.background=target;
+      var page = document.getElementsByTagName("body")[0];
+      page.style.background=target;
       alert("Congratulations. You have guessed the color\n\n" +
-          "It took you " + numberOfGuesses + "to finish the game!\n\n" +
+          "It took you " + numberOfGuesses + " guess to finish the game!\n\n" +
           "You can see the color in the background");
       finished = true;
     }
