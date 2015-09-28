99ec15d42f31f47a8f0da890368fd2231105a428
rachelbookhout <rbookhou@gmail.com>
2015-09-27 21:57:29 -0400

    outlined the matching game assignment

---
+++ Matching Game/index.js
@@ -0,0 +1,45 @@
+var numberOfFaces = 5;
+var theLeftSide = document.getElementById("leftSide");
+var theRightSide = document.getElementById("rightSide");
+var theBody = document.getElementById("body")[0];
+
+function generateFaces(){
+  //faces are created in a loop and executes numberofFaces times
+  //in each iteration an image is created using createElement()
+  //the img is the smily face
+  //the position is controlled by the top and left value
+  //top should be random between a certain range (height of div and height of image) - use style.top
+  //left should be random beween a certain range (width of div and width of image) - use style.left
+  //add the newly created image to the LeftSide dev using appendChild()
+  //call createRightSide
+  //call playGame
+}
+
+function createRightSide(){
+  //use cloneNode(true) to copy the leftSide div (leftSideImages = theLeftSide.cloneNode(true);)
+  //delete the last child of leftSideImages
+  //add leftSideImages to RightSide div
+
+}
+
+function playGame(){
+ //encorporte the two below methods for how the game is played
+ //if they are successful
+  //delete out all the child nodes of both left and right sides
+ //if they are not
+
+
+}
+
+ //add a onclick event to the last child on the left side
+  theLeftSide.lastChild.onclick= function nextLevel(event){
+        event.stopPropagation();
+        numberOfFaces += 5;
+        generateFaces();
+  };
+  //if they don't click on the last child, end the game
+  theBody.onclick = function gameOver() {
+    alert("Game Over!");
+    theBody.onclick = null;
+    theLeftSide.lastChild.onclick = null;
+  };
