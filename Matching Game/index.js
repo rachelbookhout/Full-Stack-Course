5a6a5a98ff5a87088a2e0b33f24b4589e6a977f0
rachelbookhout <rbookhou@gmail.com>
2015-10-03 19:31:29 -0400

    Code works as assigned

--- Matching Game/index.js
+++ Matching Game/index.js
@@ -1,7 +1,7 @@
 var numberOfFaces = 5;
 var theLeftSide = document.getElementById("leftSide");
 var theRightSide = document.getElementById("rightSide");
-var theBody = document.getElementById("body");
+var theBody = document.getElementsByTagName("body")[0];
 
 function generateFaces() {
 
@@ -23,58 +23,41 @@ theLeftSide.appendChild(imgface);
 
 }
 
-//createRightSide();
-
-}
-  //in each iteration an image is created using createElement()
-  //the img is the smily face
-  //the position is controlled by the top and left value
-  //top should be random between a certain range (height of div and height of image) - use style.top
-  //left should be random beween a certain range (width of div and width of image) - use style.left
-  //add the newly created image to the LeftSide dev using appendChild()
-  //call createRightSide
-  //call playGame
-
-
-function createRightSide(){
-  //use cloneNode(true) to copy the leftSide div (leftSideImages = theLeftSide.cloneNode(true);)
-  var leftSideImages = theLeftSide.cloneNode(true);
-  leftSideImages.removeChild(leftSideImages.lastChild);
-  theRightSide.appendChild(leftSide);
-  //delete the last child of leftSideImages
-  //add leftSideImages to RightSide div
-
-}
-
-function playGame(){
-  generateFaces();
- theLeftSide.lastChild.onclick= function nextLevel(event){
-        event.stopPropagation();
-        removeAllChildren();
-         numberOfFaces += 5;
-         generateFaces();
+createRightSide();
+  theLeftSide.lastChild.onclick= function nextLevel(event){
+    event.stopPropagation();
+    removingChildNodes();
+    numberOfFaces += 5;
+    generateFaces();
   };
- //encorporte the two below methods for how the game is played
- //if they are successful
-  //delete out all the child nodes of both left and right sides
- //if they are not
-theBody.onclick = function gameOver() {
+  theBody.onclick = function gameOver() {
      alert("Game Over!");
      theBody.onclick = null;
      theLeftSide.lastChild.onclick = null;
-};
+     removingChildNodes();
+  };
 
 }
 
- // //add a onclick event to the last child on the left side
- //  theLeftSide.lastChild.onclick= function nextLevel(event){
- //        event.stopPropagation();
- //        numberOfFaces += 5;
- //        generateFaces();
- //  };
- //  //if they don't click on the last child, end the game
- //  theBody.onclick = function gameOver() {
- //    alert("Game Over!");
- //    theBody.onclick = null;
- //    theLeftSide.lastChild.onclick = null;
- //  };
+function createRightSide(){
+  var leftSideImages = theLeftSide.cloneNode(true);
+  leftSideImages.removeChild(leftSideImages.lastChild);
+  theRightSide.appendChild(leftSideImages);
+}
+
+
+function removingChildNodes() {
+
+while (theLeftSide.hasChildNodes()) {
+
+  theLeftSide.removeChild(theLeftSide.firstChild);
+
+}
+
+while (theRightSide.hasChildNodes()) {
+
+  theRightSide.removeChild(theRightSide.firstChild);
+
+}
+
+}
